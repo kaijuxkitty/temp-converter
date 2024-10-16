fd469304a02e392411f501b441fb62ce844d3bbe
kaijuxkitty <kaijuxkitty@gmail.com>
2024-10-15 19:52:26 -0500

    temp-converter

---
+++ f-c.js
@@ -0,0 +1,5 @@
+let inputFahrenheit = prompt('enter temp to convert to celsius');
+let convertedCelcius = convertToCelcius(inputCelcius);
+let  celsiusTemp = (fahrenheitTemp - 32) * (5/9)
+function convertToCelcius(fahrenheit) {
+} console.log(celsiusTemp);
